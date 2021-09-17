b0b4a80b90bc56bc1a080a29ccbf5da1a88afef5
kou2911 <pnonkiti@icloud.com>
2021-09-17 20:18:58 +0900

    ユーザーの編集権限の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -54,7 +54,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user.id == current_user.id
       redirect_to action: :index
       
     end
